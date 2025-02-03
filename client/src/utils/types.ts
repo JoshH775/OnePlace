3bbf6b90124ad70fc8890fe5a7ad779b0a9755a1
Joshua Hickey <joshuah775@gmail.com>
2025-02-03 23:36:19 +0000

    syncing some frontent and backend types

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,7 +1,18 @@
 export type User = {
     id: number
     email: string
-    integrations: []
+    integrations: {
+        google?: GoogleIntegration
+    }
     createdAt: Date
+}
 
+export type GoogleIntegration = {
+    id: number;
+    userId: number;
+    googleId: string;
+    accessToken: string;
+    refreshToken: string;
+    createdAt: Date;
+    updatedAt: Date;
 }
