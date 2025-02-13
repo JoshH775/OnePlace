81be445f07334935bdb909eff2a68d94947edbfc
Joshua Hickey <joshuah775@gmail.com>
2025-02-13 19:03:01 +0000

    restyling tabs

--- client/src/components/ui/Tabs.tsx
+++ client/src/components/ui/Tabs.tsx
@@ -28,7 +28,7 @@ export function Tabs({children, tabs, selectedTab = 0, onTabChange, className = 
     }
 
     return (
-        <TabGroup selectedIndex={selectedTabIndex} onChange={onChange} className={`w-full shadow-md bg-gray-200 dark:bg-onyx-light rounded-md p-2 pt-0 ${className}`}>
+        <TabGroup selectedIndex={selectedTabIndex} onChange={onChange} className={`w-full shadow-xl dark:shadow:none  dark:bg-onyx-light rounded-md p-2 pt-0 ${className}`}>
             <TabList className="flex justify-center items-center rounded-md mb-4 p-1">
                 {tabs.map((tab, index) => (
                     <Tab key={index} className="cursor-pointer w-1/2 p-2 data-[selected]:border-b-2 data-[selected]:border-indigo font-semibold">{tab}</Tab>
