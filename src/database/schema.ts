d3f30880c4e2e037d0c85c31c0ada29f27e317c4
Joshua Hickey <joshuah775@gmail.com>
2025-02-19 00:41:24 +0000

    dropping width height and url from photo

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -11,16 +11,13 @@ const usersTable = mysqlTable('users', {
 const photosTable = mysqlTable('photos', {
   id: int().autoincrement().primaryKey(),
   userId: int().references(() => usersTable.id).notNull(),
-  url: text().notNull(),
   filename: text().notNull(),
   alias: varchar({length: 255}),
   type: varchar({length: 255}).notNull(),
   size: int().notNull(),
-  width: int().notNull(),
-  height: int().notNull(),
   location: text(),
   googleId: varchar({length: 255}).unique(),
-  date: timestamp(),
+  date: timestamp({ mode: 'date'}),
   createdAt: timestamp().defaultNow().notNull(),
   lastAccessed: timestamp().defaultNow().notNull(),
   compressed: int().notNull().default(0),
@@ -39,13 +36,10 @@ const googleIntegrationsTable = mysqlTable('google_integrations', {
 export interface Photo {
   id: number;
   userId: number;
-  url: string;
   filename: string;
   size: number;
   alias: string | null;
   type: string;
-  width: number;
-  height: number;
   location: string | null;
   date: Date | null;
   googleId: string | null;
