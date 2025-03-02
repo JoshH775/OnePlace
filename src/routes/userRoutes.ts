6af6b73ffc72d72c0c297d9efe32c2ac952d7a5d
Joshua Hickey <joshuah775@gmail.com>
2025-03-02 00:12:13 +0000

    new routes for settings

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,34 @@
 import { FastifyInstance } from "fastify";
-import { User } from "../database/repositories/UserRepository";
 import { getAllIntegrationsForUser } from "../database/repositories/IntegrationUtils";
+import SettingsRepository, { UserSettingsKeysType } from "../database/repositories/SettingsRepository";
+import { User } from "../../shared/types";
+
+const Settings = new SettingsRepository()
 
 export function registerUserRoutes(server: FastifyInstance) {
   server.get("/api/user", async (request, reply) => {
     const user = request.user as User;
     const integrations = await getAllIntegrationsForUser(user.id, false);
-    return { user, integrations };
+    const settings = await Settings.getAllForUser(user.id);
+    return { user, integrations, settings };
   });
+
+  server.get("/api/user/settings", async (request, reply) => {
+    const user = request.user as User;
+    const settings = await Settings.getAllForUser(user.id);
+    return settings;
+  });
+
+  server.post("/api/user/settings", async (request, reply) => {
+    try {
+      const user = request.user as User;
+      const { key, value } = request.body as { key: UserSettingsKeysType; value: string };
+      await Settings.setUserSetting(user.id, key, value);
+      return { success: true };
+    } catch (error) {
+      console.error(error);
+      reply.status(500);
+      return { success: false };
+  }
+})
 }
