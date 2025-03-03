c2ddd7f6fafb62ea835c78554dcb4d7796ed843d
Joshua Hickey <joshuah775@gmail.com>
2025-03-03 02:14:56 +0000

    adding props to spinner component

--- client/src/components/ui/Spinner.tsx
+++ client/src/components/ui/Spinner.tsx
@@ -1,8 +1,12 @@
-
-const Spinner = () => {
+type Props = {
+  message?: string;
+  className?: string;
+}
+const Spinner = ({ message, className = '' }: Props) => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+<div className={`flex flex-col justify-center items-center min-h-screen ${className}`}>
       <div className="animate-spin rounded-full h-16 w-16 border-4 border-indigo-600 border-t-transparent"></div>
+      {message && <p className="mt-4 text-indigo-600">{message}</p>}
     </div>
   );
 };
