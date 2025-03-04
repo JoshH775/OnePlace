f3ceae8849bd9bdfaf72e57f3cd1f4c97c12685a
Joshua Hickey <joshuah775@gmail.com>
2025-03-04 21:16:59 +0000

    Add disabled state styling to Switch component

--- client/src/components/ui/Switch.tsx
+++ client/src/components/ui/Switch.tsx
@@ -19,7 +19,9 @@ export default function Switch({checked, onChange, disabled}: Props){
             onChange={handleChange}
             className={`${
                 checked ? 'bg-indigo-600' : 'bg-gray-200'
-            } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none cursor-pointer`}
+            } ${
+                disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'
+            } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none`}
         >
             <span
                 className={`${
