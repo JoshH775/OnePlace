c5c78dc3a4ae2e102a6c9b8e65884253cc1d910a
Joshua Hickey <joshuah775@gmail.com>
2025-03-06 14:15:30 +0000

    Creating new modal for confirmations, and using it with the settings modal and reducers

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -8,6 +8,8 @@ import ToggleSetting from "./ToggleSetting";
 import { CloudIcon, MoonIcon, UserIcon } from "@heroicons/react/24/outline";
 import SettingAction from "./SettingAction";
 import IconButton from "@frontend/components/ui/IconButton";
+import { useReducer } from "react";
+import ConfirmationModal from "@frontend/components/modals/ConfirmationModal";
 
 export default function Settings() {
   const user = useAuth().user!;
@@ -32,15 +34,57 @@ export default function Settings() {
       setSettingsContext({ ...settings, [data.key]: data.value });
     },
   });
-  
 
+  interface ModalState {
+    action: () => void;
+    text: string | undefined;
+    isOpen: boolean;
+  }
+  const modalReducer = (
+    state: ModalState,
+    action: { type: string; payload?: Omit<ModalState, "isOpen"> }
+  ) => {
+    switch (action.type) {
+      case "OPEN":
+        return { ...state, isOpen: true, ...action.payload };
+      case "CLOSE":
+        return { action: () => {}, text: undefined, isOpen: false };
+      default:
+        return state;
+    }
+  };
+
+  const [modalState, dispatchModal] = useReducer(modalReducer, {
+    isOpen: false,
+    action: () => {},
+    text: undefined,
+  });
+
+  const deletePhotos = () => {
+    console.log("delete photos");
+  };
+
+  const deleteAccount = () => {
+    console.log("delete account");
+  };
 
   return (
     <div className="flex-grow flex flex-col justify-start w-full p-5">
+      <ConfirmationModal
+        isOpen={modalState.isOpen}
+        text={modalState.text}
+        onConfirm={modalState.action}
+        onClose={() => {
+          dispatchModal({ type: "CLOSE" });
+        }}
+      />
 
       <span className="justify-between w-full pb-4">
-      <p className="text-4xl font-bold">Settings</p>
-      <IconButton icon={<MoonIcon className="h-10 p-1" />} onClick={toggleDarkMode} />
+        <p className="text-4xl font-bold">Settings</p>
+        <IconButton
+          icon={<MoonIcon className="h-10 p-1" />}
+          onClick={toggleDarkMode}
+        />
       </span>
 
       <Panel className="w-full flex flex-col p-8">
@@ -56,11 +100,25 @@ export default function Settings() {
           <div className="flex justify-between items-center py-2">
             <SettingAction
               title="Google Photos"
-              subtitle={getIntegrationActionValues(integrations["google"]).status}
-              buttonText={getIntegrationActionValues(integrations["google"]).action}
-              buttonVariant={getIntegrationActionValues(integrations["google"]).buttonVariant}
-              onClick={getIntegrationActionValues(integrations["google"]).action === "Disconnect" ? () => disconnectIntegration("google") : () => {window.location.href = "/api/auth/google"}}
-              loading={isPending} />
+              subtitle={
+                getIntegrationActionValues(integrations["google"]).status
+              }
+              buttonText={
+                getIntegrationActionValues(integrations["google"]).action
+              }
+              buttonVariant={
+                getIntegrationActionValues(integrations["google"]).buttonVariant
+              }
+              onClick={
+                getIntegrationActionValues(integrations["google"]).action ===
+                "Disconnect"
+                  ? () => disconnectIntegration("google")
+                  : () => {
+                      window.location.href = "/api/auth/google";
+                    }
+              }
+              loading={isPending}
+            />
           </div>
           <ToggleSetting
             label="Automatically upload new photos to Google Photos"
@@ -71,57 +129,81 @@ export default function Settings() {
                 value: checked.toString(),
               })
             }
-            disabled={isPendingSetting || getIntegrationActionValues(integrations["google"]).action !== "Disconnect"}
+            disabled={
+              isPendingSetting ||
+              getIntegrationActionValues(integrations["google"]).action !==
+                "Disconnect"
+            }
           />
         </div>
       </Panel>
 
+      {/* ACCOUNT SETTINGS */}
       <Panel className="w-full flex flex-col mt-4 p-8">
         <span className="indigo-underline text-2xl font-bold">
           <UserIcon className="h-6 w-6 mr-2 stroke-2" />
           <p>Account</p>
         </span>
-      <p>Manage your account and data.</p>
+        <p>Manage your account and data.</p>
 
-      <SettingAction
-        title="Logout"
-        subtitle="Logout from your account and clear all session data."
-        buttonText="Logout"
-        buttonVariant="outlined"
-        onClick={logout}
-        loading={false}
-      />
+        <SettingAction
+          title="Logout"
+          subtitle="Logout from your account and clear all session data."
+          buttonText="Logout"
+          buttonVariant="outlined"
+          onClick={logout}
+          loading={false}
+        />
 
-      <SettingAction
-        title="Delete All Photos"
-        subtitle="Delete all photos from your account. This action is irreversible."
-        buttonText="Delete"
-        buttonVariant="danger"
-        onClick={() => {}}
-        loading={false}
-        className="mt-2"
-      />
+        <SettingAction
+          title="Delete All Photos"
+          subtitle="Delete all photos from your account. This action is irreversible."
+          buttonText="Delete"
+          buttonVariant="danger"
+          onClick={() => {
+            dispatchModal({
+              type: "OPEN",
+              payload: {
+                action: deletePhotos,
+                text: "Are you sure you want to delete all photos? This action is irreversible.",
+              },
+            });
+          }}
+          loading={false}
+          className="mt-2"
+        />
 
-      <SettingAction
-        title="Delete Account"
-        subtitle="Delete your account and all associated data. This action is irreversible."
-        buttonText="Delete"
-        buttonVariant="danger"
-        onClick={() => {}}
-        loading={false}
-        className="mt-2"
-      />
-
-    </Panel>
-
+        <SettingAction
+          title="Delete Account"
+          subtitle="Delete your account and all associated data. This action is irreversible."
+          buttonText="Delete"
+          buttonVariant="danger"
+          onClick={() => {
+            dispatchModal({
+              type: "OPEN",
+              payload: {
+                action: deleteAccount,
+                text: "Are you sure you want to delete your account? This action is irreversible.",
+              },
+            });
+          }}
+          loading={false}
+          className="mt-2"
+        />
+      </Panel>
     </div>
   );
 }
 
-
 //helper functions
 
-function getIntegrationActionValues(integration: GoogleClientIntegration | undefined): { status: string; action: string; buttonVariant: "filled" | "outlined" | "danger" } {
+function getIntegrationActionValues(
+  integration: GoogleClientIntegration | undefined
+): {
+  status: string;
+  action: string;
+  buttonVariant: "filled" | "outlined" | "danger";
+} {
   if (!integration) {
     return {
       status: "Not Connected",
