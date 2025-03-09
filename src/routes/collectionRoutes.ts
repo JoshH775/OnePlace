329ae9fd6e4f7878fdfd7605224d6411e5e4c37d
Joshua Hickey <joshuah775@gmail.com>
2025-03-09 18:19:43 +0000

    creating collection routes

---
+++ src/routes/collectionRoutes.ts
@@ -0,0 +1,28 @@
+import CollectionsRepository from "@backend/database/repositories/CollectionsRepository";
+import { FastifyInstance } from "fastify";
+
+const Collections = new CollectionsRepository();
+
+export function registerCollectionRoutes(server: FastifyInstance) {
+
+    server.post("/api/collections", async (req, res) => {
+        const { name, description } = req.body as { name: string, description: string };
+        const { id: userId } = req.user as { id: number };
+
+        const collection = await Collections.create({
+            name,
+            description,
+            userId,
+        });
+
+        return collection;
+    })
+
+    server.get("/api/collections", async (req, res) => {
+        const { id: userId } = req.user as { id: number };
+
+        const collections = await Collections.getCollectionsForUser(userId);
+
+        return collections;
+    })
+}
