8e1c8d14d7e93f0d11053f679ace5f6bae56c53c
Joshua Hickey <joshuah775@gmail.com>
2025-03-09 18:19:53 +0000

    create function

--- src/database/repositories/CollectionsRepository.ts
+++ src/database/repositories/CollectionsRepository.ts
@@ -4,12 +4,17 @@ import { collectionsTable } from "../schema";
 import { eq } from "drizzle-orm";
 
 export default class CollectionsRepository {
-  async createCollection(options: {
+
+  async create(options: {
     name: string;
     userId: number;
     description?: string;
-  }) {
+  }): Promise<Collection | null> {
     await db.insert(collectionsTable).values(options);
+    const collection = await db.query.collectionsTable.findFirst({
+      where: (collection, { and, eq }) => and(eq(collection.name, options.name), eq(collection.userId, options.userId)),
+    });
+    return collection ?? null;
   }
 
   async getCollectionsForUser(userId: number): Promise<Collection[] | null> {
@@ -25,7 +30,7 @@ export default class CollectionsRepository {
     }
   }
 
-  async getCollectionById(id: number, userId: number): Promise<Collection | null> {
+  async getById(id: number, userId: number): Promise<Collection | null> {
     const collection = await db.query.collectionsTable.findFirst({
       where: (collection, { and, eq }) => and(eq(collection.userId, userId), eq(collection.id, id)),
     });
@@ -33,7 +38,7 @@ export default class CollectionsRepository {
     return collection ?? null;
   }
 
-  async updateCollection(options: ProtoCollection & { id: number }): Promise<boolean> {
+  async update(options: ProtoCollection & { id: number }): Promise<boolean> {
     try {
       await db.update(collectionsTable).set(options).where(eq(collectionsTable.id, options.id));
       return true;
