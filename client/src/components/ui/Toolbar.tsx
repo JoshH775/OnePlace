4c0783296254c80a72bd6e302cbbe21759c75464
Joshua Hickey <joshuah775@gmail.com>
2025-03-14 19:15:52 +0000

    removing animations from toolbar

--- client/src/components/ui/Toolbar.tsx
+++ client/src/components/ui/Toolbar.tsx
@@ -3,20 +3,21 @@ import React from "react";
 type Props = {
   children: React.ReactNode;
   isOpen: boolean;
-  loading: boolean;
+  loading?: boolean;
+  className?: string;
 };
 
-const Toolbar = ({ children, isOpen, loading }: Props) => {
+const Toolbar = ({ children, isOpen, loading = false, className = '' }: Props) => {
   return (
-    <div
+    <>
+    {isOpen && <div
       id="toolbar"
-      className={`flex items-center justify-center shadow-lg gap-6 w-fit rounded-full border dark:bg-onyx dark:border-onyx-light p-2 ease-out px-7 absolute transition-all left-1/2 -translate-x-1/2 bottom-15 ${
-        isOpen ? "scale-100 opacity-100" : "scale-80 opacity-0"
-      } ${loading ? "bg-gray-400 cursor-not-allowed" : ""}`}
+      className={`flex items-center justify-center shadow-lg gap-6 w-fit rounded-full border dark:bg-onyx dark:border-onyx-light p-2 ease-out px-7 absolute left-1/2 -translate-x-1/2 bottom-15 ${loading ? "bg-gray-400 cursor-not-allowed" : ""} ${className}`}
     >
       {children}
-    </div>
-  );
+    </div>}
+    </>)
+    
 };
 
 export default Toolbar;
