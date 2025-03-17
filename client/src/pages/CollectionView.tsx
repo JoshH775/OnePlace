2feafab085c67cc3de6903d778e0e6664fde5055
Joshua Hickey <joshuah775@gmail.com>
2025-03-17 13:31:42 +0000

    deleting collections THIS IS STILL WONKY

--- client/src/pages/CollectionView.tsx
+++ client/src/pages/CollectionView.tsx
@@ -1,14 +1,21 @@
+import ConfirmationModal from "@frontend/components/modals/ConfirmationModal";
 import PhotoGallery from "@frontend/components/PhotoGallery";
+import Button from "@frontend/components/ui/Button";
 import Spinner from "@frontend/components/ui/Spinner";
 import api from "@frontend/utils/api";
 import { Collection } from "@shared/types";
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
+import { Trash2 } from "lucide-react";
+import { Suspense, useState } from "react";
+import toast from "react-hot-toast";
 import { useNavigate, useParams } from "react-router-dom";
 
 export default function CollectionView() {
   const { id } = useParams();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const [confirmModalOpen, setConfirmModalOpen] = useState(false);
 
   if (!id) {
       navigate("/collections");
@@ -18,12 +25,7 @@ export default function CollectionView() {
     queryKey: ["collection", id],
     queryFn: async () => {
       const response = await api.req(`/collections/${id}`);
-      if (response.status !== 200 || !response.data) {
-        if (response.status === 404) {
-          throw new Error("Collection not found");
-        }
-        throw new Error("Error fetching collection");
-      }
+      if (response.status !== 200 || !response.data) return null
 
       const collection = response.data as Collection;
 
@@ -32,7 +34,31 @@ export default function CollectionView() {
     },
   });
 
-  console.log(data);
+  const { mutateAsync: deleteMutation } = useMutation({
+    mutationFn: async () => {
+        toast.loading('Deleting collection...')
+        const { status } = await api.req(`/collections/${id}`, { method: "DELETE" });
+        console.log(status, 'status')
+        if (status !== 200) {
+            toast.error('Error deleting collection')
+            throw new Error("Error deleting collection");
+        }
+        else {
+            console.log('returning status')
+            return status
+        }
+
+    },
+    onSuccess: () => {
+            console.log('RJARJAJRAJRJARJA')
+            queryClient.invalidateQueries({ queryKey: ["collections"] });
+            toast.success("Collection deleted!");
+            navigate("/collections");
+    }})
+
+    
+    
+
 
   return (
     <Suspense fallback={<Spinner />}>
@@ -42,9 +68,19 @@ export default function CollectionView() {
         </div>
       ) : (
         <div className="w-full flex-grow flex flex-col p-5">
-          <p className="text-3xl font-bold indigo-underline mb-3">
+        <ConfirmationModal isOpen={confirmModalOpen} onClose={() => setConfirmModalOpen(false)} onConfirm={deleteMutation} />
+
+            <span className="justify-between w-full">
+            <p className="text-3xl font-bold indigo-underline mb-3">
             {data.collection.name}
           </p>
+          <Button
+            variant="danger"
+            className="!w-fit rounded-md"
+            onClick={() => {setConfirmModalOpen(true)}}
+            ><Trash2 className="h-6 w-6 text-white" /> Delete Collection </Button>
+            </span>
+
           <p className="dark:text-subtitle-dark text-subtitle-light mb-2">
             {data.collection.description}
           </p>
