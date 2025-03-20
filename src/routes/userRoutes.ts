106ebf6005170b68793bb4b1523f8392a112ac2b
Joshua Hickey <joshuah775@gmail.com>
2025-03-20 16:39:51 +0000

    including tags in user data object

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,15 +2,18 @@ import { FastifyInstance } from "fastify";
 import { getAllIntegrationsForUser } from "../database/repositories/IntegrationUtils";
 import SettingsRepository from "../database/repositories/SettingsRepository";
 import { User, UserSettingsKeysType } from "../../shared/types";
+import TagsRepository from "@backend/database/repositories/TagsRepository";
 
 const Settings = new SettingsRepository()
+const Tags = new TagsRepository()
 
 export function registerUserRoutes(server: FastifyInstance) {
   server.get("/api/user", async (request, reply) => {
     const user = request.user as User;
     const integrations = await getAllIntegrationsForUser(user.id, false);
     const settings = await Settings.getAllForUser(user.id);
-    return { user, integrations, settings };
+    const tags = await Tags.getTagsForUser(user.id);
+    return { user, integrations, settings, tags };
   });
 
   server.get("/api/user/settings", async (request, reply) => {
