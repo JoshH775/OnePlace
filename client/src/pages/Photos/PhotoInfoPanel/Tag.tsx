f2d10f8f134367305d22c8f87685466aeb1e19c5
Joshua Hickey <joshuah775@gmail.com>
2025-03-25 19:57:11 +0000

    Tag CRUD in frontend

--- client/src/pages/Photos/PhotoInfoPanel/Tag.tsx
+++ client/src/pages/Photos/PhotoInfoPanel/Tag.tsx
@@ -1,4 +1,6 @@
+import api from "@frontend/utils/api"
 import type { Tag } from "@shared/types"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { X } from "lucide-react"
 
 type Props = {
@@ -7,10 +9,24 @@ type Props = {
 }
 
 export default function Tag({ tag, edit }: Props) {
+
+    const photoId = Number(sessionStorage.getItem("photoId"))
+
+    const queryClient = useQueryClient()
+
+    const { mutateAsync: deleteTagMutation } = useMutation({
+        mutationFn: async () => api.photos.removeTagFromPhoto(photoId, tag.id)
+    })
+
+    const deleteTag = async () => {
+        const newTags = await deleteTagMutation()
+        queryClient.setQueryData(["photoTags", photoId], newTags)
+    }
+
     return (
-        <div className="tag">
-            <span className="font-semibold text-center flex gap-2 text-white px-2 py-1 bg-indigo text-sm rounded-full" style={{ backgroundColor: tag.color || ''}}>{tag.name}
-                {edit && <X className="w-3.5 h-3.5 cursor-pointer" />}
+        <div className="tag" >
+            <span className="font-semibold text-center flex gap-2 text-white px-2 py-1 text-sm rounded-full items-center" style={{ backgroundColor: tag.color || '#4f39f6'}}>{tag.name}
+                {edit && <X className="w-3.5 h-3.5 cursor-pointer" onClick={() => edit && deleteTag()}/>}
             </span>
         </div>
     )
