46c72f88f688e09fa96a2bd2f1e2fa4026689c1a
Joshua Hickey <joshuah775@gmail.com>
2025-04-11 11:00:11 +0100

    Created endpint to fetch last accessed

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,9 +3,11 @@ import { getAllIntegrationsForUser } from "../database/repositories/IntegrationU
 import SettingsRepository from "../database/repositories/SettingsRepository";
 import { User, UserSettingsKeysType } from "../../shared/types";
 import TagsRepository from "@backend/database/repositories/TagsRepository";
+import { UsersRepository } from "@backend/database/repositories/UserRepository";
 
-const Settings = new SettingsRepository()
-const Tags = new TagsRepository()
+const Settings = new SettingsRepository();
+const Tags = new TagsRepository();
+const Users = new UsersRepository();
 
 export function registerUserRoutes(server: FastifyInstance) {
   server.get("/api/user", async (request, reply) => {
@@ -22,9 +24,19 @@ export function registerUserRoutes(server: FastifyInstance) {
   });
 
   server.post("/api/user/settings", async (request, reply) => {
-      const user = request.user as User;
-      const { key, value } = request.body as { key: UserSettingsKeysType; value: string };
-      await Settings.setUserSetting(user.id, key, value);
-      return reply.status(204)
-})
+    const user = request.user as User;
+    const { key, value } = request.body as {
+      key: UserSettingsKeysType;
+      value: string;
+    };
+    await Settings.setUserSetting(user.id, key, value);
+    return reply.status(204);
+  });
+
+  server.get("/api/user/last-accessed", async (request, reply) => {
+    const user = request.user as User;
+    const lastAccessed = await Users.getLastAccessed(user.id);
+
+    return lastAccessed;
+  });
 }
