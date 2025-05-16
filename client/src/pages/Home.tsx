afc4d49ce775a2324a3f7cc47b8ce67399ed6335
Joshua Hickey <joshuah775@gmail.com>
2025-05-16 12:13:31 +0100

    Minor home screen page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,36 @@
-import NoPhotos from "./Photos/NoPhotos"
-
+import Button from "@frontend/components/ui/Button";
+import { useNavigate } from "react-router-dom";
+import { FolderIcon, Images, SettingsIcon } from "lucide-react";
 
 export default function Home() {
+  const navigate = useNavigate();
 
+  return (
+    <div className="flex w-full">
+      <div className="flex-grow flex items-center justify-center w-full h-full">
+        <div className="flex flex-col items-center justify-center gap-3 border border-gray-300 dark:border-onyx-light h-fit rounded-md p-12 max-w-[575px]">
+          <h1 className="text-2xl font-bold">Welcome!</h1>
+          <p className="text-center">
+            This is your homepage. You can explore your photos, browse
+            collections, or adjust your user settings.
+          </p>
 
-    return (
-        <div className="flex w-full">
+          <Button onClick={() => navigate("/photos")}>
+            <Images className="h-7" />
+            Explore Photos
+          </Button>
 
-            <NoPhotos />
+          <Button onClick={() => navigate("/collections")} variant="outlined">
+            <FolderIcon className="h-7" />
+            Browse Collections
+          </Button>
 
+          <Button onClick={() => navigate("/settings")} variant="outlined">
+            <SettingsIcon className="h-7" />
+            User Settings
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
